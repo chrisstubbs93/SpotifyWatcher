adc2b62f7fb8e310d04682dd9a6bb8474e734ca3
Chris <chris@chris-stubbs.co.uk>
2013-03-29 15:05:05 +0000

    Working

---
+++ SpotifyWatcher/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 62)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Click to start monitoring spotify"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Spotify is playing:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(145, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Red
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button2.Location = New System.Drawing.Point(156, 62)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "BLOCK THIS S41T!"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Unblock a song"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(156, 91)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(121, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Broken it?"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(288, 127)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "SpotifyWatcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ SpotifyWatcher/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim songdata(1) As String
+    Dim title As String
+    Dim blocks(1024) As String
+    Dim ptr As Integer
+    Dim FILE_NAME As String = "C:\spotifywatcherblocks.txt"
+
+    Dim spotify As New spotify()
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Button1.Text = "SpotifyWatcher Running"
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim p As Process
+        Dim p2() As Process
+        p2 = Process.GetProcessesByName("spotify")
+        For Each p In Process.GetProcessesByName("spotify")
+
+            title = p.MainWindowTitle.ToString
+            If title.Length() > 10 Then
+                songdata = Split(p.MainWindowTitle.ToString, " - ")
+                songdata = Split(songdata(1), " – ")
+                Label2.Text = songdata(0)
+                Label3.Text = songdata(1)
+
+                For a = 0 To blocks.Length - 1
+                    If blocks(a) = title Then
+                        ' MsgBox("BLOCKED SONG")
+
+
+                        spotify.PlayNext()
+
+
+                    End If
+                Next
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Button1.Text = "SpotifyWatcher Running"
+        Button1.Enabled = False
+
+        '  Dim objWriter As New System.IO.StreamWriter(FILE_NAME, False)
+        readfile()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim aryText As String
+        aryText = title
+        Dim objWriter As New System.IO.StreamWriter(FILE_NAME, True)
+        objWriter.WriteLine(aryText)
+        objWriter.Close()
+        ' MsgBox("Text Appended to the File")
+        readfile()
+
+    End Sub
+
+    Sub readfile()
+        Try
+            ' Create an instance of StreamReader to read from a file.
+            Dim sr As StreamReader = New StreamReader(FILE_NAME)
+            Dim line As String
+            ' Read and display the lines from the file until the end 
+            ' of the file is reached.
+            Do
+                line = sr.ReadLine()
+                ' MsgBox(line)
+                blocks(ptr) = line
+                ptr += 1
+            Loop Until line Is Nothing
+            sr.Close()
+        Catch
+            ' Let the user know what went wrong.
+            '  MsgBox("The file could not be read:")
+        End Try
+    End Sub
+
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        MsgBox("Delete the appropriate line from this notepad file")
+        MsgBox("Make sure you leave the blank newline at the end of the file")
+        System.Diagnostics.Process.Start(FILE_NAME)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        MsgBox("Broken the program? Try exiting, deleting '" & FILE_NAME & "', then opening this program again")
+    End Sub
+End Class
